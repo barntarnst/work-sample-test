7222a06041112c3dbf1a42bd3e6e2199fc9634c0
Sravan <vsravan707@gmail.com>
2018-09-17 23:03:54 +0200

    added hot reloading

--- src/index.js
+++ src/index.js
@@ -6,11 +6,23 @@ import registerServiceWorker from './utils/registerServiceWorker';
 import './globalStyles/index.css';
 import App from './App';
 import configureStore from './store';
+
 /*eslint-disable */
+
+const rootEl = document.getElementById('root');
+
 ReactDOM.render(
   <Provider store={configureStore()}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  rootEl,
 );
 registerServiceWorker();
+
+// hot reloading
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(<NextApp />, rootEl);
+  });
+}
